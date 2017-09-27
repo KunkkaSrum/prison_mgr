49494823b7c6b623bbeae7a2bbb42aad7a3a4f26
KunkkaSrum <huangyh9511@163.com>
2017-09-27 13:06:56 +0800

    Initial commit

--- src/main/java/com/yixing/prison_mgr/services/LoginService.java
+++ src/main/java/com/yixing/prison_mgr/services/LoginService.java
@@ -6,7 +6,6 @@ import com.yixing.prison_mgr.entity.StudentEntity;
  * Created by Master_Q on 2017/9/20.
  */
 public interface LoginService {
-
     StudentEntity queryByNameAndPassword(String name,String password);
 
 }
